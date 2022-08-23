90791a0df222a816a230cb2afdabc21e8faa6ad4
Mdbotz <111772910+Mdbotz@users.noreply.github.com>
2022-08-24 00:16:28 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -10,10 +10,10 @@ async def add_caption(bot, message):
     await db.set_caption(user_id, text[1])
     await message.reply_text("<b>caption added successfully</b>")
     
-@Client.on_message(filters.command('show_caption'))
+@Client.on_message(filters.command('showcaption'))
 async def show_caption(bot, message):
     user_id = message.from_user.id
     caption = await db.get_caption(user_id)
     if not caption:
        return await message.reply_text("You didn't added any custom caption")
-    await message.reply_text(f"<b>Your Caption:\n\n`{caption}`")
+    await message.reply_text(f"<b>Your Custom Caption:</b>\n\n`{caption}`")
